958dcb5880fa57c1c87434696a657636be98f0ba
juan.mosquerap <juan.mosquerap@udea.edu.co>
2026-03-31 14:34:57 -0500

    Initial commit

---
+++ src/main/java/com/paymentplatform/embeddedpayments/EmbeddedPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.paymentplatform.embeddedpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymentplatform/embeddedpayments/EmbeddedPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentplatform.embeddedpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
